b72127f1e0a9c2d0d79a9758624597110b133648
Cosmic-rare <ysolakey1116@gmail.com>
2022-09-21 20:27:04 +0900

    Clean4

--- src/module/apply.js
+++ src/module/apply.js
@@ -1,6 +1,13 @@
 import db from "../db/db";
 import _ from "lodash";
 
+const applied = (val) => {
+  db.sync.update(val._syncId, {
+    ...val,
+    _applied: 1,
+  });
+};
+
 export default (sync) => {
   sync?.map(async (val) => {
     let task = await db.tasks.get(val.command.p[0]);
@@ -8,32 +15,22 @@ export default (sync) => {
     switch (val.command.op) {
       case "create":
         db.tasks.put({ ...val.command.oi, _id: val.command.p[0] });
-        db.sync.update(val._syncId, {
-          ...val,
-          _applied: 1,
-        });
+        applied(val);
         break;
 
       case "update":
-        console.log(task[val.command.p[1]]);
         if (task[val.command.p[1]] === val.command.ou[0]) {
           task[val.command.p[1]] = val.command.ou[1];
           task._updatedAt = val.command.updatedAt;
           db.tasks.update(val.command.p[0], task);
-          db.sync.update(val._syncId, {
-            ...val,
-            _applied: 1,
-          });
+          applied(val);
         }
         break;
 
       case "delete":
         if (_.isEqual(val.command.od, task)) {
           db.tasks.delete(task._id);
-          db.sync.update(val._syncId, {
-            ...val,
-            _applied: 1,
-          });
+          applied(val);
         }
     }
   });
